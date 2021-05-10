3ac3080ae3f9b7fff0dcd5be09f2e843f329c09d
Dominik Kröll <dominik.kroell@mni.thm.de>
2021-05-10 21:46:30 +0200

    delete

--- src/main/kotlin/de/thm/mni/microservices/gruppe6/servicelib/ServiceException.kt
+++
@@ -1,9 +0,0 @@
-package de.thm.mni.microservices.gruppe6.servicelib
-
-import org.springframework.http.HttpStatus
-
-class ServiceException(message: String) : Exception(message) {
-
-    val httpStatus: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR
-
-}
