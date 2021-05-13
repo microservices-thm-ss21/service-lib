49871fd32e88a98163d0e8e709a796e4bbcbee6a
nhrt <hartingerniklas@gmail.com>
2021-05-13 10:19:40 +0200

    Removed serializable

--- lib/src/main/kotlin/de/thm/mni/microservices/gruppe6/lib/event/ServiceEvent.kt
+++ lib/src/main/kotlin/de/thm/mni/microservices/gruppe6/lib/event/ServiceEvent.kt
@@ -1,8 +1,6 @@
 package de.thm.mni.microservices.gruppe6.lib.event
 
-import java.io.Serializable
-
 data class ServiceEvent(
     val eventCode: EventCode,
-    val content: Serializable
-    ): Serializable
+    val content: String
+    )
