17217328fbc4595090af3691093b34afc6f0a431
Dominik Kröll <dominik.kroell@mni.thm.de>
2021-06-17 01:20:32 +0200

    Cleanup

--- lib/src/main/kotlin/de/thm/mni/microservices/gruppe6/lib/exception/ServiceException.kt
+++ lib/src/main/kotlin/de/thm/mni/microservices/gruppe6/lib/exception/ServiceException.kt
@@ -6,13 +6,8 @@ import org.springframework.web.server.ResponseStatusException
 /**
  * Exception which maps the HttpStatus and is being handled by the ResponseStatusExceptionResolver to send a HTTP Response
  */
-class ServiceException: ResponseStatusException {
-    constructor() : this(HttpStatus.INTERNAL_SERVER_ERROR)
-    constructor(cause: Throwable) : this(HttpStatus.INTERNAL_SERVER_ERROR, cause)
-    constructor(reason: String?) : this(HttpStatus.INTERNAL_SERVER_ERROR, reason)
-    constructor(reason: String?, cause: Throwable?) : this(HttpStatus.INTERNAL_SERVER_ERROR, reason, cause)
-    constructor(httpStatus: HttpStatus) : super(httpStatus)
-    constructor(httpStatus: HttpStatus, cause: Throwable) : this(httpStatus, cause.message, cause)
-    constructor(httpStatus: HttpStatus, reason: String?) : super(httpStatus, reason)
-    constructor(httpStatus: HttpStatus, reason: String?, cause: Throwable?) : super(httpStatus, reason, cause)
-}
+class ServiceException(
+    httpStatus: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+    reason: String? = null,
+    cause: Throwable? = null
+) : ResponseStatusException(httpStatus, reason, cause)
