e1b60fd601c498c7633b8fc3e217132229b91281
Dominik Kröll <Dominik.Kroell@mni.thm.de>
2021-07-18 15:56:03 +0200

    Streamline dependencies. All Services use the same Spring & Kotlin version now.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.20"
-    java
+    id("org.springframework.boot") version "2.4.7"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.4.32"
+    kotlin("plugin.spring") version "1.4.32"
     `maven-publish`
 }
 
@@ -33,16 +35,14 @@ dependencies {
 
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    api("org.springframework.security:spring-security-core:5.4.6")
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc:2.4.5")
-
-
+    api("org.springframework.security:spring-security-core")
+    implementation("org.springframework.data:spring-data-r2dbc")
 
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
     // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.springframework.boot:spring-boot-starter-webflux:2.4.5")
+    api("org.springframework.boot:spring-boot-starter-webflux")
     api("io.jsonwebtoken:jjwt-impl:0.11.2")
     api("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
