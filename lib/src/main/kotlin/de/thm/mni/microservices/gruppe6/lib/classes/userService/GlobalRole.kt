4efc9c79a02a16f06962585cf6ee7d41ef56dc88
Dominik Kröll <Dominik.Kroell@mni.thm.de>
2021-07-21 18:24:36 +0200

    USer Management

--- lib/src/main/kotlin/de/thm/mni/microservices/gruppe6/lib/classes/userService/GlobalRole.kt
+++ lib/src/main/kotlin/de/thm/mni/microservices/gruppe6/lib/classes/userService/GlobalRole.kt
@@ -5,8 +5,7 @@ import org.springframework.security.core.GrantedAuthority
 enum class GlobalRole: GrantedAuthority {
     USER,
     ADMIN,
-    SUPPORT,
-    NORMAL;
+    SUPPORT;
 
     override fun getAuthority(): String = this.name
 }
