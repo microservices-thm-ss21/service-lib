a5ad985229f8559380b258e97f0b68e2a3f0dce7
Jannis Weber <jannis.weber@mni.thm.de>
2021-08-03 17:31:55 +0200

    add covering of unexpected exceptions

---
+++ lib/src/main/kotlin/de/thm/mni/microservices/gruppe6/lib/exception/Util.kt
@@ -0,0 +1,18 @@
+package de.thm.mni.microservices.gruppe6.lib.exception
+
+import org.springframework.http.HttpStatus
+
+/**
+ * Covers all unexpected exceptions.
+ * Removes the error message and changaes httpStatus to internalServerError.
+ * A Exception is unexpected if it is not an instance of ServiceException.
+ * @param exception
+ * @return coveredException
+ */
+fun coverUnexpectedException(exception: Throwable): Throwable{
+    return if (exception !is ServiceException) {
+        ServiceException(HttpStatus.INTERNAL_SERVER_ERROR)
+    } else {
+        exception
+    }
+}
