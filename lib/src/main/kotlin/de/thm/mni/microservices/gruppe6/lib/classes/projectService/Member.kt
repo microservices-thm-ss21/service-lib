914f904d3cb59d798d433d6be701a06d8c6482d8
Jannis Weber <jannis.weber@mni.thm.de>
2021-08-03 15:05:28 +0200

    hide Member.id (only used for storage purposes)

--- lib/src/main/kotlin/de/thm/mni/microservices/gruppe6/lib/classes/projectService/Member.kt
+++ lib/src/main/kotlin/de/thm/mni/microservices/gruppe6/lib/classes/projectService/Member.kt
@@ -1,12 +1,13 @@
 package de.thm.mni.microservices.gruppe6.lib.classes.projectService
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.util.*
 
 @Table("members")
 data class Member(
-    @Id var id: UUID?,
+    @Id @JsonIgnore var id: UUID?,
     var projectId: UUID,
     var userId: UUID,
     var projectRole: String
